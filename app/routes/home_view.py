89839286dcd26be5a229e13cbb84626fd99e2a8b
Syeed <nuruddinsayeed@gmail.com>
2022-06-14 01:40:36 +0600

    views and template structure added

--- app/routes/home_view.py
+++ app/routes/home_view.py
@@ -1,15 +1,14 @@
 '''
-File: monitor_view.py
+File: home_view.py
 Project: SportsSafety
-File Created: Friday, 10th June 2022 2:49:13 am
+File Created: Tuesday, 14th June 2022 1:35:44 am
 Author: Syeed (nur.syeed@stud.fra-uas.de, nuruddinsayeed14@gmail.com)
 -----
-Last Modified: Tuesday, 14th June 2022 1:39:39 am
+Last Modified: Tuesday, 14th June 2022 1:35:46 am
 Modified By: Syeed (nur.syeed@stud.fra-uas.de>)
 -----
 Copyright 2022 - 2022 This Module Belongs to Open source project
 '''
-
 
 from fastapi import APIRouter
 from starlette.templating import Jinja2Templates
@@ -19,13 +18,16 @@ router = APIRouter()
 templates = Jinja2Templates("app/templates")
 
 
-@router.get("/") # TODO: add depends
-def monitor_home():
-    return {"Message": "Hello world"}
-
-# Activity Monitor Page
-@router.get("/monitor", name="login")
+# Login Page
+@router.get("/login", name="login")
 async def index(request: Request):
     """Renders the login page"""
 
-    return templates.TemplateResponse('monitor.html', {'request': request})
+    return templates.TemplateResponse('login.html', {'request': request})
+
+
+@router.get("/register", name="register")
+async def index(request: Request):
+    """Renders the register page"""
+
+    return templates.TemplateResponse('register.html', {'request': request})
--- tests/test_data_sending.py
+++ tests/test_data_sending.py
@@ -6,12 +6,12 @@ import asyncio
 async def send_to_ws():
     async with websockets.connect('ws://127.0.0.1:8000/ws/running/sayed') as websocket:
 
-        for i in range(10):
+        for i in range(30):
             # await websocket.send(json.dumps({'value':random.randint(1,100)}))
             await websocket.send(json.dumps({
-                'x':random.randint(1,100),
-                'y':random.randint(1,100),
-                'z': random.randint(1,100)
+                'x':random.randint(1,100)/10,
+                'y':random.randint(1,100)/10,
+                'z': random.randint(1,100)/10
             }))
 
             response = await websocket.recv()
